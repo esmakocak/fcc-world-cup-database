4984ac9edef1b4b2f814c0588cfc8ca532d4d39b
Esma Koçak <101140922+esmakocak@users.noreply.github.com>
2023-05-08 01:50:45 +0300

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -8,7 +8,6 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-
 echo $($PSQL "TRUNCATE TABLE games, teams")
  
 
@@ -23,11 +22,11 @@ do
     if [[ -z $WINNER_ID ]]
     then
       # insert team_id
-      INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
-      if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
-      then
-        echo "Inserted into teams, $WINNER"
-      fi
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      # if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]]
+      # then
+      #   echo "Inserted into teams, $WINNER"
+      # fi
     fi
 
       # get team_id
@@ -37,18 +36,17 @@ do
       if [[ -z $OPPONENT_ID ]]
       then
         # insert team_id
-        INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
-        if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
-        then
-          echo "Inserted into teams, $OPPONENT" 
-        fi
+        echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+        # if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]]
+        # then
+        #   echo "Inserted into teams, $OPPONENT" 
+        # fi
       fi
 
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND' , $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+    echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND' , $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
 
   fi
 
 done
-
